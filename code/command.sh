50b5e763074496b6dc7248b374b2ffaa58ec6c31
Gabriel Simoes Gaspar <gabrielsgaspar@gmail.com>
2020-08-13 15:03:35 +0100

    Define project function

---
+++ code/command.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function project () {
+    cd "/Users/gabrielsgaspar/Projects/personal_projects/terminal_automation/code"
+    source .env
+    python instructions.py $1
+    cd $PROJECT_PATH/$1
+    git init
+    git remote add origin https://github.com/$USER/$1.git
+    touch README.md
+    touch .gitignore
+    git add .
+    git commit -m "Initial commit"
+    git push -u origin master
+    atom .
+}
